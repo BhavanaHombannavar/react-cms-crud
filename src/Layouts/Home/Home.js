bd7d6448eabe31784bd893e8600462fdbebcaa64
Bhavana Hombannavar <h.bhavana@webandappstudio.com>
2021-09-28 20:24:22 +0530

    first posts section loaded

--- src/Layouts/Home/Home.js
+++ src/Layouts/Home/Home.js
@@ -1,10 +1,54 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Card from './../../components/Card/Card';
 import CardStyle2 from './../../components/CardStyle2/CardStyle2';
 import styles from './Home.module.css';
 
-const Home = () => (
-  <>
+/*let posts=[];
+
+posts = new Posts().getPostsList((data)=>{
+  posts = data
+});
+console.log(posts)*/
+
+function Home (){
+
+  // const [posts, setPosts] = useState([]);
+  const [renderposts, setRenderPosts] = useState([]);
+  const [updateState, setUpdateState] = useState(false);
+
+ useEffect(()=>{
+   let posts1 = renderposts;
+   let prevId = 0;
+   fetch('https://jsonplaceholder.typicode.com/posts/')
+    .then(response=>response.json())
+    .then((json) => {
+        // console.log(json)
+        const promises = json.map((obj) => {
+          // console.log(obj.userId)
+          if(obj.userId !== prevId){
+            let url = `https://jsonplaceholder.typicode.com/users/${obj.userId}`
+            // console.log(url)
+            return fetch(url)
+                .then(response=>response.json())
+                .then((json) => {
+                    // console.log(json);
+                    posts1.push({post:obj, user:json});
+                    setRenderPosts(posts1);
+                    // console.log(renderposts)
+                })
+                prevId = obj.userId;
+          }
+        })
+
+        return Promise.all(promises)
+    })
+    .then(() => {
+        console.log('job done')
+        setUpdateState(true);
+    })
+ }, [updateState]);
+  return (<>
+  // {console.log(renderposts)}
   <section className={styles.banner}>
     <div className="container">
       <h1>Hello ReactJs</h1>
@@ -14,25 +58,14 @@ const Home = () => (
   <section>
   <div className="container">
   <div className="row">
-  <div className="col-md-4">
-    <Card/>
+    {renderposts.map((posts, index)=>{
+         if(index<6){
+           return <div className="col-md-4" key={posts.post.id}>
+            <Card post={posts}/>
+          </div>
+        }
+    })}
   </div>
-  <div className="col-md-4">
-    <Card/>
-  </div>
-  <div className="col-md-4">
-    <Card/>
-  </div>
-  <div className="col-md-4">
-    <Card/>
-  </div>
-  <div className="col-md-4">
-    <Card/>
-  </div>
-  <div className="col-md-4">
-    <Card/>
-  </div>
-</div>
 </div>
 </section>
 <section className="section2">
@@ -50,5 +83,5 @@ const Home = () => (
 </section>
 </>
 );
-
+}
 export default Home;
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import styles from './Card.module.css';
 
-function Card(){
+function Card(post){
   return (
     <div className={`${styles.card} card`}>
        <div className={styles.infos}>
-        <p className={styles.title}>sunt aut facere repellat provident occaecati excepturi optio reprehenderit</p>
-         <p className={styles.text}>uia et suscipit\nsuscipit recusandae consequuntur expedita et cum nreprehenderit </p>
-          <span className={styles.date}>August 12, 2021 | Author</span>
+        <p className={styles.title}>{post.post.post.title}</p>
+         <p className={styles.text}>{post.post.post.body.length < 80 ? post.post.post.body : post.post.post.body.slice(0, 75)+'...'} </p>
+          <span className={styles.date}>August 12, 2021 | {post.post.user.name}</span>
         <span className={styles.read_more}>Read More <i className="fa fa-long-arrow-right"></i></span>
       </div>
     </div>
